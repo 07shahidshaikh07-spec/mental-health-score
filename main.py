f2e4bca5dfdf5ffb6399f9feefd9510e44a6f713
Shahid Shaikh <07shahidshaikh07@gmail.com>
2026-09-04 16:28:12 +0530

    Fix CORS for deployed frontend

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://mental-health-score-3-vn3u.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
